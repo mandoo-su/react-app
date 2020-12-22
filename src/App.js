173e9f8de3f1570dccce0473bc477a54bc75ec37
mandoo-su <mandoo-su@users.noreply.github.com>
2020-12-23 00:03:23 +0900

    201222

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
-import logo from './logo.svg';
+//mport logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 
-function App() {
+
+
+class App extends Component{
+  constructor(props){
+    //component가 실행될때 constructor 가 가장 앞에 있으면 제일 먼저 실행되서 초기화 담당
+    super(props);
+    this.state={
+      mode:'welcome',
+      subject:{title:'WEB',sub:'World Woide Web! WWW'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for desing'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
+  render(){
+    //props나 state가 바뀌면 화면이 다시 그려진다. render 다시 호출
+    var _title, _desc = null;
+    if(this.state.mode==='welcome'){
+      _title=this.state.welcome.title;
+      _desc=this.state.welcome.desc;
+    }else if(this.state.mode==='read'){
+      
+    }
+    return(
+      //title="WEB" sub="world wide handsome web!"></Subject>
+    <div className="App">
+      <Subject 
+        title={this.state.subject.title}
+        sub={this.state.subject.sub}>
+      </Subject>
+      <TOC data={this.state.contents}></TOC>
+      <Content title={_title} desc={_desc}></Content>
+    </div>
+    );
+  }
+}
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +64,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
---
+++ src/components/Subject.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Subject extends Component{
+    render(){
+      return (//header 1개의 최상위 태그만 사용 
+        <header> 
+        <h1><a href="/">{this.props.title}</a></h1>
+        {this.props.sub}
+        </header>
+      ); 
+    }
+  }
+
+  export default Subject; 
