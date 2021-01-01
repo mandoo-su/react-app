fd994c5232254639f655c895114bf94598363d1e
mandoo-su <mandoo-su@users.noreply.github.com>
2021-01-01 23:59:00 +0900

    React 다시 끄적끄적_내일 이벤트

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import Subject from "./components/Subject";
 
 
 
-class App extends Component{
+class App extends Component{//App 이라는 컴포넌트 
   constructor(props){
     //component가 실행될때 constructor 가 가장 앞에 있으면 제일 먼저 실행되서 초기화 담당
     super(props);
     this.state={
-      mode:'welcome',
+      mode:'read',
       subject:{title:'WEB',sub:'World Woide Web! WWW'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
-      contents:[
+      contents:[//데이터가 여러개 
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for desing'},
         {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
@@ -23,13 +23,15 @@ class App extends Component{
     }
   }
   render(){
+    console.log('App render');
     //props나 state가 바뀌면 화면이 다시 그려진다. render 다시 호출
     var _title, _desc = null;
     if(this.state.mode==='welcome'){
       _title=this.state.welcome.title;
       _desc=this.state.welcome.desc;
     }else if(this.state.mode==='read'){
-      
+      _title=this.state.contents[0].title;
+      _desc=this.state.contents[0].desc; 
     }
     return(
       //title="WEB" sub="world wide handsome web!"></Subject>
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 
 class Subject extends Component{
     render(){
+      console.log('Subject render');
       return (//header 1개의 최상위 태그만 사용 
         <header> 
-        <h1><a href="/">{this.props.title}</a></h1>
-        {this.props.sub}
+        <h1><a href="/">{this.props.title}</a>
+        </h1>{this.props.sub}
         </header>
       ); 
     }
