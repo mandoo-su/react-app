351c64319020cae5a740aa63f0c731fa95fafe30
mandoo-su <mandoo-su@users.noreply.github.com>
2021-01-05 21:16:07 +0900

    create구현: shouldComponentUpdate 공부

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 
 class TOC extends Component{
+    shouldComponentUpdate(newProps, newState){
+      //부모 component가 호출되면 무조건 자식 component의 render도 다 호출 되는걸 방지, false
+      console.log('==>TOC reander'
+      ,newProps.data//바뀐 정보
+      ,this.props.data//이전의 정보
+      );
+      if(this.props.data===newProps.data){
+        return false;
+        //false로 하면 자식 component는 부모 component의 state가 바뀌어도 render 되지 않기 때문에 create한거 바로 목록에 표시X
+      }
+      return true;
+    }
     render(){
       console.log('TOC render');
       var lists=[];
